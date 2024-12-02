67e56a1335420a0f6f7c2d2eccdd33156a7be0ea
Greg Dahlman <dahlman@gmail.com>
2024-12-02 16:05:10 -0700

    Day:1 Task:1 file IO checkpoint

---
+++ 2024/python/aoc/day1task1.py
@@ -0,0 +1,26 @@
+def distance(left, right):
+    return abs(left-right)
+
+def total_distance(left,right):
+    res = 0
+    left.sort()
+    right.sort()
+    for index in range(len(left)):
+        res += distance(left[index],right[index])
+
+    return res
+
+def read_file(fname):
+    with open(fname) as f:
+        lines = f.read().splitlines()
+    left=[]
+    right=[]
+    for line in lines:
+        lval,rval = line.split()
+        left.append(int(lval))
+        right.append(int(rval))
+    return(left,right)
+
+def run_it(fname):
+    lvalues, rvalues = read_file(fname)
+    return total_distance(lvalues,rvalues)
