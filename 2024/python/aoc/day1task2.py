0461dccbc7452115c257b153a3176fa7d0042a13
Greg Dahlman <dahlman@gmail.com>
2024-12-03 10:52:35 -0700

    day2task2 red

---
+++ 2024/python/aoc/day1task2.py
@@ -0,0 +1,33 @@
+
+def similarity(left,right):
+    pass
+
+def total_distance(left, right):
+    res = 0
+    left.sort()
+    right.sort()
+    for index in range(len(left)):
+        res += distance(left[index], right[index])
+
+    return res
+
+
+def read_file(fname):
+    with open(fname) as f:
+        lines = f.read().splitlines()
+    left = []
+    right = []
+    for line in lines:
+        lval, rval = line.split()
+        left.append(int(lval))
+        right.append(int(rval))
+    return (left, right)
+
+
+def run_it(fname):
+    lvalues, rvalues = read_file(fname)
+    return total_distance(lvalues, rvalues)
+
+
+if __name__ == "__main__":
+    print(run_it("data/input/1.txt")) # pragma: no cover
---
+++ 2024/python/tests/test_day1_task2.py
@@ -0,0 +1,26 @@
+from aoc.day1task2 import similarity,read_file,run_it
+
+def test_similarity():
+    left = [3,4,2,1,3,3]
+    right = [4,3,5,3,9,3]
+    items = [
+        [3,4],
+        [4,3],
+        [2,5],
+        [1,3],
+        [3,9],
+        [3,3]
+    ]
+    res = similarity(left,right)
+    assert res == 31
+
+
+def test_read_file():
+    lres, rres = read_file('data/test/1.txt')
+    assert lres == [3,4,2,1,3,3]
+    assert rres == [4,3,5,3,9,3]
+
+def test_run_it():
+    #res = run_it('data/test/1.txt')
+    #assert res == 11
+    assert True
