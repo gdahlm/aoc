44474a9a5f2499f017d745a6138e7a3d1682ecf8
Greg Dahlman <dahlman@gmail.com>
2024-12-08 00:43:49 -0700

    day8 part 1 done

--- 2024/python/aoc/day8.py
+++ 2024/python/aoc/day8.py
@@ -1,6 +1,25 @@
 """AoC 2024 Day 8"""
 
 
+# To avoid imports, copy of tertools.product
+def product(*iterables, repeat=1):
+    """
+    product('ABCD', 'xy') → Ax Ay Bx By Cx Cy Dx Dy
+    product(range(2), repeat=3) → 000 001 010 011 100 101 110 111
+    """
+
+    if repeat < 0:
+        raise ValueError("repeat argument cannot be negative")
+    pools = [tuple(pool) for pool in iterables] * repeat
+
+    result = [[]]
+    for pool in pools:
+        result = [x + [y] for x in result for y in pool]
+
+    for prod in result:
+        yield tuple(prod)
+
+
 # File reading and cleaning
 def fread_line(filename):
     """Lazy read of file returning a generator"""
@@ -47,6 +66,31 @@ def double_distance(ref_point, given_point):
     return new_x, new_y
 
 
-def find_antinodes(board, antennas):
-    pass
-    # TODO
+def valid_point(point, board):
+    ROWS, COLS = len(board), len(board[0])
+    r, c = point
+    if r < 0 or c < 0 or r >= ROWS or c >= COLS:
+        return False
+    return True
+
+
+def find_antinodes(antennas, board):
+    res = set()
+    for key in antennas:
+        pairs = list(product(antennas[key], repeat=2))
+        for pair in pairs:
+            left, right = pair
+            if left == right:
+                continue
+            else:
+                _ = double_distance(left, right)
+                if valid_point(_, board):
+                    res.add(_)
+                _ = double_distance(right, left)
+                if valid_point(_, board):
+                    res.add(_)
+    return len(res)
+
+
+# rdata = [x.strip() for x in fread_line('data/input/8.txt')]
+# test_it(find_antennas(rdata), rdata)
--- 2024/python/tests/test_day8.py
+++ 2024/python/tests/test_day8.py
@@ -1,8 +1,11 @@
 from aoc.day8 import (
     fread_line,
+    product,
     find_antennas,
     find_antinodes,
     known_antinodes,
+    valid_point,
+    double_distance,
 )
 
 test = [
@@ -53,12 +56,28 @@ def test_fread_line():
 
 
 def test_find_antennas():
-    assert find_antennas(test) == {'0': [(1, 8), (2, 5), (3, 7), (4, 4)], 'A': [(5, 6), (8, 8), (9, 9)]}
-    assert find_antennas(test_2) == {'a': [(3, 4), (5, 5)]}
+    assert find_antennas(test) == {
+        "0": [(1, 8), (2, 5), (3, 7), (4, 4)],
+        "A": [(5, 6), (8, 8), (9, 9)],
+    }
+    assert find_antennas(test_2) == {"a": [(3, 4), (5, 5)]}
+
 
 def test_find_known_antinodes():
-    assert known_antinodes(test_2) == {'#': [(1, 3), (7, 6)]}
+    assert known_antinodes(test_2) == {"#": [(1, 3), (7, 6)]}
 
-def test_find_antinodes():
+
+def test_double_distance():
+    assert double_distance((5, 5), (3, 4)) == (1, 3)
+    assert double_distance((3, 4), (5, 5)) == (7, 6)
+
+
+def test_valid_move():
     assert True
     # TODO
+
+
+def test_find_antinodes():
+    tdata = [x.strip() for x in fread_line("data/test/8.txt")]
+    res = find_antinodes(find_antennas(tdata), tdata)
+    assert res == 14
