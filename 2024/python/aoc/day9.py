8970a8fd20f97feb7a9e1ba5404ee585c0cfd7b6
Greg Dahlman <dahlman@gmail.com>
2024-12-09 13:47:07 -0700

    add csv functions for expantion and checksum

--- 2024/python/aoc/day9.py
+++ 2024/python/aoc/day9.py
@@ -10,13 +10,17 @@
 
 
 """
+
+import re
 from io import StringIO
 
-test_input = "2333133121414131402" # pylint: disable=invalid-name
+test_input = "2333133121414131402"  # pylint: disable=invalid-name
 
-test_input_expanded = "00...111...2...333.44.5555.6666.777.888899" # pylint: disable=invalid-name
+test_input_expanded = (
+    "00...111...2...333.44.5555.6666.777.888899"  # pylint: disable=invalid-name
+)
 
-test_input_sorting_steps = [    # pylint: disable=invalid-name
+test_input_sorting_steps = [  # pylint: disable=invalid-name
     "00...111...2...333.44.5555.6666.777.888899",
     "009..111...2...333.44.5555.6666.777.88889.",
     "0099.111...2...333.44.5555.6666.777.8888..",
@@ -32,9 +36,9 @@ test_input_sorting_steps = [    # pylint: disable=invalid-name
     "0099811188827773336446555566..............",
 ]
 
-small_input = "12345" # pylint: disable=invalid-name
+small_input = "12345"  # pylint: disable=invalid-name
 
-small_input_expanded = "0..111....22222" # pylint: disable=invalid-name
+small_input_expanded = "0..111....22222"  # pylint: disable=invalid-name
 
 small_input_sorting_steps = [
     "0..111....22222",
@@ -64,22 +68,99 @@ def expand_disk(string: str) -> str:
         if f_value is not None:
             f_out = "." * f_value
         else:
-            f_out = ''
-        
-        sentence.write(  b_out + f_out)
+            f_out = ""
+
+        sentence.write(b_out + f_out)
+
+    return sentence.getvalue()
+
+def expand_disk_csv(string: str) -> str:
+    sentence = StringIO()
+    blocks = string[::2]
+    freespace = string[1::2]
+    lenght = max(len(blocks), len(freespace))
+
+    for index in range(lenght):
+        f_value = None
+        b_count = int(blocks[index])
+        b_value = index
+
+        if index < len(freespace):
+            f_value = int(freespace[index])
+
+        b_value =str(b_value) + ","
+        b_out = (b_value * b_count)
+
+        if f_value is not None:
+            f_out = ".," * f_value
+        else:
+            f_out = ""
+
+        sentence.write(b_out + f_out)
 
     return sentence.getvalue()
 
 def calculate_checksum(string: str) -> int:
     res = 0
     for index, item in enumerate(string):
-        if item != '.':
+        if item != ".":
             res += int(item) * index
     return res
 
+def calculate_checksum_csv(string: str) -> int:
+    res = 0
+    array = string.split(',')
+    for index, item in enumerate(array):
+        if item != ".":
+            res += int(item) * index
+    return res
+
+
 def sort_disk(string: str) -> str:  # pylint: disable=unused-argument
     # TODO
-    pass
+    array = [x for x in string]
+    num_pattern = re.compile(r"\d")
+    free_pattern = re.compile(r"(\.+)")
+    free_matches = re.finditer(free_pattern, string)
+    num_matches = re.finditer(num_pattern, "".join(array)[::-1])
+    length = len(array)
+
+    for free_match in free_matches:
+        start, end = free_match.span()
+        for index in range(start, end):
+            num_match = next(num_matches)
+            match_val = num_match.group(0)
+            _, match_index = num_match.span()
+            match_index *= -1
+            offset = length + match_index
+            if index >= offset:
+                break
+            array[index], array[match_index] = match_val, "."
+    return "".join(array)
+
+def sort_disk_csv(csv_string: str) -> str:  # pylint: disable=unused-argument
+    # TODO
+    array = csv_string.split(',')
+    num_pattern = re.compile(r"\,(\d)")
+    free_pattern = re.compile(r"\,(\.+)")
+    free_matches = re.finditer(free_pattern, string)
+    num_matches = re.finditer(num_pattern, "".join(array)[::-1])
+    length = len(array)
+
+    for free_match in free_matches:
+        start, end = free_match.span()
+        for index in range(start, end):
+            num_match = next(num_matches)
+            match_val = num_match.group(0)
+            _, match_index = num_match.span()
+            match_index *= -1
+            offset = length + match_index
+            if index >= offset:
+                break
+            array[index], array[match_index] = match_val, "."
+    return "".join(array)
+
+
 
 def main() -> None:
     """Main function"""
--- 2024/python/tests/test_day9.py
+++ 2024/python/tests/test_day9.py
@@ -2,6 +2,7 @@
 import pytest
 from aoc.day9 import ( # pylint: disable=import-error
     expand_disk,
+    expand_disk_csv,
     calculate_checksum,
     main,
 )
@@ -15,7 +16,18 @@ def test_expand_disk():
 
     for test in expansion_tests:
         assert expand_disk(test[0]) == test[1]
+        
+def test_expand_disk_csv():
+    expansion_tests = [
+        ['233313312141413140233',
+        '0,0,.,.,.,1,1,1,.,.,.,2,.,.,.,3,3,3,.,4,4,.,5,5,5,5,.,6,6,6,6,.,7,7,7,.,8,8,8,8,9,9,.,.,.,10,10,10,'],
+        ['12345', '0,.,.,1,1,1,.,.,.,.,2,2,2,2,2,' ],
+        ['2333133121414131402', '0,0,.,.,.,1,1,1,.,.,.,2,.,.,.,3,3,3,.,4,4,.,5,5,5,5,.,6,6,6,6,.,7,7,7,.,8,8,8,8,9,9,' ],
+    ]
 
+    for test in expansion_tests:
+        assert expand_disk_csv(test[0]) == test[1]
+ 
 def test_calculate_checksum():
     data = '0099811188827773336446555566..............'
     res = 1928
