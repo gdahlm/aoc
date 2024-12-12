23836aa09caeb48d23523f56ae3d8741d5142dbb
Greg Dahlman <dahlman@gmail.com>
2024-12-12 09:00:43 -0700

    Day12t2 minor cleanup

---
+++ 2024/python/aoc/day12.py
@@ -0,0 +1,14 @@
+"""AoC 2024 Day12 Task 1"""
+
+# File reading
+def fread_all(file_path: str) -> list[str]:
+    """Return all lines from a file at once"""
+    with open(file_path, "r", encoding="utf-8") as file_in:
+        return file_in.readlines()
+
+def main() -> None:
+    """Main function"""
+    #TODO
+
+if __name__ == "__main__":
+    main()
