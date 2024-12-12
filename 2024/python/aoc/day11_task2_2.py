4488f8ce734be1fd38f7b164adc3213cade3eb99
Greg Dahlman <dahlman@gmail.com>
2024-12-11 21:24:23 -0700

    Start Day 11 part 2 refactor

---
+++ 2024/python/aoc/day11_task2_2.py
@@ -0,0 +1,8 @@
+"""AoC 2024 Day 11
+
+1) if stone == 0, then stone == 1
+2) if len(stone) % 2 == 0:
+    split in left, right in the middle, with leading zeros removed
+3) stone replace with new stone, old val * 2024
+
+"""
