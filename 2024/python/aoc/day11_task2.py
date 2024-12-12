57f004112dacc08f2395c9b356c3d8afccbf14d3
Greg Dahlman <dahlman@gmail.com>
2024-12-11 20:06:19 -0700

    partial fix

--- 2024/python/aoc/day11_task2.py
+++ 2024/python/aoc/day11_task2.py
@@ -1,22 +1,16 @@
 """AoC 2024 Day 11
 
-1) If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-2) If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-3)If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
-
-
 1) if stone == 0, then stone == 1
 2) if len(stone) % 2 == 0:
     split in left, right in the middle, with leading zeros removed
 3) stone replace with new stone, old val * 2024
 
 """
-from functools import lru_cache 
+#from functools import lru_cache
 
 def stones_to_int(stone: str)-> list[int]:
     return [int(x) for x in stone.split()]
 
-#@lru_cache(maxsize = 1024) 
 def stone_action(stone:int ):
 
     if not isinstance(stone, int):
@@ -42,6 +36,7 @@ def stone_action(stone:int ):
     yield stone*2024
 
 def do_blink_gen(stones):
+
     if isinstance(stones, list):
         _ = {}
         for item in stones:
@@ -52,8 +47,15 @@ def do_blink_gen(stones):
         stones = _
 
     for key in stones:
-        for _ in range(stones[key]):
-            yield from stone_action(key)
+        multiplier = 1
+        if stones[key] > 1:
+            multiplier = stones[key]
+
+        res = list(stone_action(key))
+        res = res * multiplier
+
+        
+        yield from res
 
 def do_blinks(iterations: int, stones):
     res = stones
@@ -76,7 +78,7 @@ def do_blinks_map(stones):
     blink_gen = do_blink_gen(stones)
     for item in blink_gen:
         if item in found:
-           found[item] +=1 
+            found[item] +=1
         else:
             found[item] = 1
 
@@ -84,11 +86,11 @@ def do_blinks_map(stones):
 
 
 def count_stones(iterations: int, stones) -> int:
-    sum = 0
+    total = 0
     res = do_blinks(iterations, stones)
     for index in res:
-        sum += res[index]
-    return sum
+        total += res[index]
+    return total
 
 
 
