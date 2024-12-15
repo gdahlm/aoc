849c2a8706f7029b797c13198b7af25e82d4777f
Greg Dahlman <dahlman@gmail.com>
2024-12-14 22:25:22 -0700

    Day 15 file tests checkpoint

--- 2024/python/aoc/day15.py
+++ 2024/python/aoc/day15.py
@@ -4,6 +4,20 @@ from dataclasses import dataclass
 
 
 #File handling
-def fread_line(file_name: str):
+def read_file(file_name: str):
     with open(file_name, "r", encoding="utf-8") as file_in:
         return [line.strip() for line in file_in]
+
+def clean_data(filename):
+    raw_input = read_file(filename)
+    board = []
+    moves = []
+    for line in raw_input:
+        match line:
+            case "":
+                pass
+            case x if '#' in line:
+                board.append(line)
+            case _:
+                moves.append(line)
+
--- 2024/python/tests/test_day15.py
+++ 2024/python/tests/test_day15.py
@@ -1,11 +1,13 @@
 """Tests for AoC 2024 Day 12"""
 
 import pytest            # pylint: disable=unused-import
-from aoc.day12 import (  # pylint: disable=import-error
-    main,
+from aoc.day15 import (  # pylint: disable=import-error
+    read_file,
+    clean_data,
 )
 
+def test_fread_all():
+    assert len(read_file('data/test/15_small.txt')) == 10
 
-def test_main():
-    assert main() is None
-    # TODO
+def test_clean_data():
+    assert True
