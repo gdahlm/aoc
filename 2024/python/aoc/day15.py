b94b0693a11b80adf0783383b7e4da1deea43c2b
Greg Dahlman <dahlman@gmail.com>
2024-12-14 23:11:52 -0700

    Minor formating fixes

--- 2024/python/aoc/day15.py
+++ 2024/python/aoc/day15.py
@@ -1,14 +1,23 @@
 """AoC 2024 Day 15 Task 1"""
-import re
+
 from dataclasses import dataclass
 
 
-#File handling
+@dataclass
+class Point:
+    """Generic point data object"""
+
+    x: int
+    y: int
+
+
+# File handling
 def read_file(file_name: str) -> list[str]:
     with open(file_name, "r", encoding="utf-8") as file_in:
         return [line.strip() for line in file_in]
 
-def clean_data(filename:str) -> list[list[str]]:
+
+def clean_data(filename: str) -> list[list[str]]:
     raw_input = read_file(filename)
     board = []
     moves = []
@@ -16,36 +25,47 @@ def clean_data(filename:str) -> list[list[str]]:
         match line:
             case "":
                 pass
-            case x if '#' in line:
+            case x if "#" in line:  # pylint: disable=W0612
                 board.append(line)
             case _:
                 moves.append(line)
     return board, moves
 
-def parse_move(move:chr) -> tuple:
+
+def parse_move(move: chr) -> tuple:
     """Return move tuple"""
     match move:
-        case '^':
-            print('Up')
-            return((-1,0))
-        case 'v':
-            print('Down')
-            return((1,0))
-        case '>':
-            print('Right')
-            return((0,1))
-        case '<':
-            print('Left')
-            return((0,-1))
+        case "^":
+            print("Up")
+            return (-1, 0)
+        case "v":
+            print("Down")
+            return (1, 0)
+        case ">":
+            print("Right")
+            return (0, 1)
+        case "<":
+            print("Left")
+            return (0, -1)
 
-def look_ahead(location:tuple, move_mask:tuple):
-    #TODO
+
+def look_ahead(location: tuple, move_mask: tuple):
+    """See if boxes can move"""
+    # TODO
     return None
+
 
 def move_boxes():
-    #TODO
+    """Move boxes"""
+    # TODO
     return None
 
+
 def score_it():
-    #TODO
+    """Calculate score based on 'GPS' weights"""
+    # TODO
     return None
+
+
+if __name__ == "__main__":
+    print("TODO")
--- 2024/python/tests/test_day15.py
+++ 2024/python/tests/test_day15.py
@@ -1,6 +1,6 @@
 """Tests for AoC 2024 Day 12"""
 
-import pytest            # pylint: disable=unused-import
+import pytest  # pylint: disable=unused-import
 from aoc.day15 import (  # pylint: disable=import-error
     read_file,
     clean_data,
@@ -10,32 +10,37 @@ from aoc.day15 import (  # pylint: disable=import-error
     score_it,
 )
 
+
 def test_fread_all():
     """Test file reading"""
-    raw_input = read_file('data/test/15_small.txt')
+    raw_input = read_file("data/test/15_small.txt")
     assert len(raw_input) == 10
-    assert raw_input[1] == '#..O.O.#'
+    assert raw_input[1] == "#..O.O.#"
+
 
 def test_clean_data():
     """Test file parsing"""
-    board, moves = clean_data('data/test/15_small.txt')
-    assert moves == ['<^^>>>vv<v>>v<<']
-    assert board[1] == '#..O.O.#'
+    board, moves = clean_data("data/test/15_small.txt")
+    assert moves == ["<^^>>>vv<v>>v<<"]
+    assert board[1] == "#..O.O.#"
     assert len(board) == 8 and len(board[0]) == 8
 
 
 def test_parse_move():
     # TODO
-    assert parse_move(None) == None
+    assert parse_move(None) is None
+
 
 def test_look_ahead():
     # TODO
-    assert look_ahead(None, None) == None
+    assert look_ahead(None, None) is None
+
 
 def test_move_boxes():
-    #TODO
-    assert move_boxes() == None
+    # TODO
+    assert move_boxes() is None
+
 
 def test_score_it():
-    #TODO
-    assert score_it() == None
+    # TODO
+    assert score_it() is None
