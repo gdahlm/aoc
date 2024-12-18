90b4176f3a1017687e80169f27ace2a07e970ad2
Greg Dahlman <dahlman@gmail.com>
2024-12-18 13:23:37 -0700

    Day17 extract to functions

--- 2024/python/aoc/day17.py
+++ 2024/python/aoc/day17.py
@@ -1,73 +1,74 @@
 """AoC 2024 Day 17 Task 1"""
-def vm(**kwargs):
-    # Registers
-    A, B ,C = None, None, None #pylint: disable=C0103
-    # pointer
-    pointer = 0
-    output = None
+from dataclasses import dataclass, field
 
-    def combo_operands(operand:int) -> int:
-        nonlocal A, B, C #pylint: disable=C0103
-        match operand:
-            case 1 | 2 | 3 as n:
-                return n
-            case 4:
-                return A
-            case 5:
-                return B
-            case 6:
-                return C
-            case 7:
-                raise ValueError('Combo Operand (7) is Reserved')
-            case _:
-                raise ValueError('Combo Operand is out of range')
+@dataclass(slots=True)
+class MachineState:
+    """Generic point data object"""
+    # registers
+    a: int
+    b: int
+    c: int
+    #instruction pointer
+    pointer: int
+    #output
+    output: list[int] = field(default_factory=list)
 
-    def do_instruction(opcode:int, operand:int):
-        nonlocal A,B,C,pointer,output #pylint: disable=C0103
-        match opcode:
-            case 0:
-                #adv:
-                pointer += 2
-                A = A//combo_operands(operand)**2 #pylint: disable=C0103
-            case 1:
-                #bxl:
-                pointer += 2
-                B = B ^ operand #pylint: disable=C0103
-            case 2:
-                #bst
-                pointer += 2
-                B = combo_operands(operand) % 8 #pylint: disable=C0103
-            case 3:
-                #jnz:
-                if A == 0: #pylint: disable=C0103
-                    pointer += 2
-                else:
-                    pointer = operand
-            case 4:
-                #bxc:
-                pointer += 2
-                B = B ^ C #pylint: disable=C0103
-            case 5:
+
+def combo_operands(operand:int) -> int:
+    match operand:
+        case 0| 1 | 2 | 3 as n:
+            return int(n)
+        case 4:
+            return state.a
+        case 5:
+            return state.b
+        case 6:
+            return state.c
+        case 7:
+            raise ValueError('Combo Operand (7) is Reserved')
+        case _:
+            raise ValueError('Combo Operand is out of range')
+
+def do_instruction(opcode:int, operand:int):
+    match opcode:
+        case 0:
+            #adv:
+            state.pointer += 2
+            state.a = state.a//combo_operands(operand)**2 #pylint: disable=C0103
+        case 1:
+            #bxl:
+            state.pointer += 2
+            state.b = state.b ^ operand #pylint: disable=C0103
+        case 2:
+            #bst
+            state.pointer += 2
+            state.b = combo_operands(operand) % 8 #pylint: disable=C0103
+        case 3:
+            #jnz:
+            if state.a == 0: #pylint: disable=C0103
+                state.pointer += 2
+            else:
+                state.pointer = operand
+        case 4:
+            #bxc:
+            state.pointer += 2
+            state.b = state.b ^ state.c #pylint: disable=C0103
+        case 5:
                 #out:
-                pointer += 2
-                output.append(combo_operands(operand) %8)
-            case 6:
-                #bdv:
-                pointer += 2
-                B = A//combo_operands(operand)**2 #pylint: disable=C0103
-            case 6:
-                #bdv:
-                pointer += 2
-                B = A//combo_operands(operand)**2 #pylint: disable=C0103
+            state.pointer += 2
+            state.output.append(combo_operands(operand) %8)
+        case 6:
+            #bdv:
+            state.pointer += 2
+            state.b = state.a//(combo_operands(operand)**2) #pylint: disable=C0103
+        case 6:
+            #bdv:
+            state.pointer += 2
+            state.b = state.a//combo_operands(operand)**2 #pylint: disable=C0103
+#    return state
 
-    def inner():
-        nonlocal A, B, C, pointer, output #pylint: disable=C0103
-        print(f'A:{A} B:{B} C:{C}')
+def print_vm():
+    print(f'A:{state.a} B:{state.b} C:{state.c} Instruction Pointer: {state.pointer}')
 
-    #public methods
-    inner.combo_operands = combo_operands
 
-    # Exposed Attributes
-    inner.A, inner.B, inner.C, inner.pointer, = A, B, C, pointer
-
-    return inner
+state = MachineState(0,0,0,0,[])
