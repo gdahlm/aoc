0d1f8df439c736af7fd736272aa658fa5c0779aa
Greg Dahlman <dahlman@gmail.com>
2024-12-19 19:56:46 -0700

    Day 19 done

--- 2024/python/aoc/day19.py
+++ 2024/python/aoc/day19.py
@@ -2,14 +2,16 @@
 
 # pylint: disable=E0606,E0601,C0103
 
+
 # File Handeling
 def read_file(file_name: str) -> list[str]:
     with open(file_name, "r", encoding="utf-8") as file_in:
         return [line.strip() for line in file_in]
 
-def clean_data(filename:str):
+
+def clean_data(filename: str):
     raw_input = read_file(filename)
-    towels = raw_input[0].split(', ')
+    towels = raw_input[0].split(", ")
 
     patterns = []
     for index in range(2, len(raw_input)):
@@ -17,57 +19,63 @@ def clean_data(filename:str):
 
     return towels, patterns
 
-# Trie based prefix tree
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_word = False
 
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word: str) -> bool:
-        node = self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.is_word = True
-
-    def search(self, word: str) -> bool:
-        node = self.root
-        for char in word:
-            if char not in node.children:
-                return False
-            node = node.children[char]
-        return node.is_word
-
-    def starts_with(self, prefix: str) -> bool:
-        node = self.root
-        for char in prefix:
-            if char not in node.children:
-                return False
-            node = node.children[char]
+def can_construct(pattern: str, towels: list[str], memo=None) -> bool:
+    if memo is None:
+        memo = {}
+    if pattern in memo:
+        return memo[pattern]
+    # Base Case
+    if pattern == "":
         return True
 
-def build_prefix_tree(words:list[str]) -> object:
-    obj = Trie()
-    for word in words:
-        obj.insert(word)
-    return obj
+    for towel in towels:
+        if pattern.find(towel) == 0:
+            suffix = pattern[len(towel) :]
+            if can_construct(suffix, towels, memo=memo):
+                memo[pattern] = True
+                return True
+
+    memo[pattern] = False
+    return False
+
+
+def count_construct(pattern: str, towels: list[str], memo=None) -> int:
+    total = 0
+    if memo is None:
+        memo = {}
+    if pattern in memo:
+        return memo[pattern]
+    # Base Case
+    if pattern == "":
+        return 1
+
+    for towel in towels:
+        if pattern.find(towel) == 0:
+            suffix = pattern[len(towel) :]
+            number_matches = count_construct(suffix, towels, memo)
+            total += number_matches
+            memo[suffix] = number_matches
+    memo[pattern] = total
+    return total
+
+
+def solution(filename):
+    towels, patterns = clean_data(filename)
+    task1, task2 = 0, 0
+    # Task 1
+    for pattern in patterns:
+        task1 += can_construct(pattern, towels)
+
+    # Task 2
+    for pattern in patterns:
+        task2 += count_construct(pattern, towels)
+
+    return task1, task2
 
 
 if __name__ == "__main__":
 
-    towels, patterns = clean_data('data/test/19.txt')
-    trie = build_prefix_tree(towels)
-
-    # Example of trying to match 'brwrr'
-    print(trie.starts_with("b"))
-    print(trie.starts_with("br"))
-    print(trie.starts_with("brw"))
-    print(trie.starts_with("w"))
-    print(trie.starts_with("wr"))
-    print(trie.starts_with("wrr"))
-    print(trie.starts_with("r"))
+    task_1, task_2 = solution("data/input/19.txt")
+    print(f"Task 1: {task_1}")
+    print(f"Task 2: {task_2}")
--- 2024/python/tests/test_day19.py
+++ 2024/python/tests/test_day19.py
@@ -3,5 +3,41 @@
 # pylint: disable=E0606,C0116,W0611
 import pytest  # pylint: disable=unused-import
 from aoc.day19 import (  # pylint: disable=import-error
+    read_file,
+    clean_data,
+    can_construct,
+    count_construct,
+    solution,
 )
 
+test_filename = "data/test/19.txt"  # pylint: disable=invalid-name
+
+# Load data
+towels, patterns = clean_data(test_filename)
+
+
+def test_read_file():
+    assert read_file(test_filename)
+
+
+def test_clean_data():
+    assert isinstance(towels, list)
+    assert isinstance(towels[0], str)
+    assert isinstance(patterns, list)
+    assert isinstance(patterns[0], str)
+
+
+def test_can_construct():
+    assert can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"])
+    assert not can_construct("abcxdef", ["ab", "abc", "cd", "def", "abcd"])
+
+
+def test_count_construct():
+    assert count_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]) == 1
+    assert count_construct("abzcdef", ["ab", "abc", "cd", "def", "abcd"]) == 0
+
+
+def test_solution():
+    task1, task2 = solution(test_filename)
+    assert task1 == 6
+    assert task2 == 16
